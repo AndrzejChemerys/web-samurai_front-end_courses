50ddc7f2af1545cf2ccfeb2fc6be826bda4b24b0
AndriiChemerys <andriichemerys@gmail.com>
2018-10-02 20:50:56 +0200

    opening bottom menu with arrow button and js

--- Section_13/les7/script.js
+++ Section_13/les7/script.js
@@ -1,3 +1,11 @@
+//const arrow = document.querySelector(".arrow");
+//const nav = document.querySelector("nav");
+//
+//arrow.addEventListener("click", function () {
+//    arrow.classList.toggle("on");
+//    nav.classList.toggle("on");
+//})
+
 const arrow = document.querySelector(".arrow");
 const nav = document.querySelector("nav");
 
