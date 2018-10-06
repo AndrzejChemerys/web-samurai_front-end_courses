1e45a745b9b380b3cb76d002d3815cee1e637586
AndriiChemerys <andriichemerys@gmail.com>
2018-10-06 15:42:40 +0200

    adding first animation

--- Section_14/les6/code.js
+++ Section_14/les6/code.js
@@ -1,1 +1,20 @@
-
+$(document).on('scroll', function() {
+    
+    const windowHeight = $(window).height();
+    const scrollValue = $(this).scrollTop();
+//    console.log(scrollValue);
+    const $art2 = $('.art2');
+    const art2FromTop = $art2.offset().top;
+//    console.log(art1FromTop);
+    const art2Height = $art2.outerHeight();
+    console.log(art2Height);
+    
+    if (scrollValue > art2FromTop + art2Height - windowHeight) {
+        console.log("jestem");
+        $art2.addClass("active");
+    }
+    
+    if(scrollValue < 100) {
+        $('article').removeClass('active');
+    }
+})
