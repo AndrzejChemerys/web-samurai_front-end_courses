80548c819ab88dc539a1352ccff48fb51394a880
AndriiChemerys <andriichemerys@gmail.com>
2019-02-03 18:35:23 +0100

    animation with text changing

--- JavaScript_programming_course/sec_10/les4/main.js
+++ JavaScript_programming_course/sec_10/les4/main.js
@@ -8,21 +8,34 @@ const spnText = document.querySelector('.text');
 const spnCursor = document.querySelector('.cursor');
 const txt = ['tekst1', 'tekst2', 'tekst3']
 
-let indexTextX = 0;
-let indexTextY = 0;
+let activeLetter = -15;
+let activeText = 0;
+
 
 // Implementacja
 const addLetter = () => {
-//  setTimeout(clearInterval(spnText),600);
-    spnText.textContent += txt[indexTextX][indexTextY];
-    indexTextY++;
-    if (indexTextY===txt[indexTextX].length)  {
-        indexTextX++;
-        if (indexTextX===txt.length) return;
-        indexTextY=0;
-    }
-    setTimeout(addLetter, 200);
+ // Użyj w środku setTimeout
+ if (activeLetter >= 0) {
+  spnText.textContent += txt[activeText][activeLetter];
  }
+ activeLetter++;
+ if (activeLetter === txt[activeText].length) {
+
+  activeText++;
+  if (activeText === txt.length) return;
+
+  return setTimeout(() => {
+   activeLetter = -15;
+   spnText.textContent = '';
+   addLetter();
+  }, 2000)
+
+
+ }
+ setTimeout(addLetter, 100)
+
+}
+
 
 addLetter(); //pierwsze wywołanie
 
