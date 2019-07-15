3b077d471ecaf2edc29364a1a8d463aadef3404a
AndriiChemerys <andriichemerys@gmail.com>
2019-07-15 20:07:37 +0200

    Learning state and props objects

---
+++ 3_react_from_beginning_course/sec3/les4/app.js
@@ -0,0 +1,27 @@
+class ShoppingList extends RTCIceCandidate.Component {
+
+    state= {
+        items1: 'tomatos',
+        items2: 'juice',
+        items3: 'something to drink'
+    }
+
+    render () {
+        return(
+            <>
+            <h1>Lista zakupów</h1>
+            <ul>
+                <ItemList name={this.state.items1} example={2+2}/>
+                <ItemList name={this.state.items2}/>
+                <ItemList name={this.state.items3}/>
+            </ul>
+            </>
+        )
+    }
+}
+
+const ItemList = (props) => {
+    return (
+        <li>{this.props.name}-{this.props.example}</li>
+    )
+}
