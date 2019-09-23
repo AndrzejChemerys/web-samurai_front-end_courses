d68854f7388c1765561a39d58079164b39fd4e66
AndriiChemerys <andriichemerys@gmail.com>
2019-09-23 20:15:34 +0200

    adding basic js

--- 4_advanced_projects_in_CSS_and_JS/sec9_project_1/index.js
+++ 4_advanced_projects_in_CSS_and_JS/sec9_project_1/index.js
@@ -1,10 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log('Hello world!');
 
-    const rootElement=document.querySelector('#root');
+    const rootElement = document.querySelector('#root');
     const sections = document.querySelectorAll('sections');
+    let currentSectionIndex = 0;
 
-    document.addEventListener('mousewheel', function(event){
+    document.addEventListener('mousewheel', function (event) {
+        const direction = event.wheelDelta < 0 ? 1 : -1;
+        if (direction === 1) {
+            const isLastection = currentSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if (firstSection) return;
+        }
+        currentSectionIndex = currentSectionIndex + direction;
         console.log(event.whellDelta)
     })
 })
