4b9d77970b7e732a212cc18db860243a5cee30b9
AndriiChemerys <andriichemerys@gmail.com>
2019-09-24 20:29:40 +0200

    add scroll feature

--- 4_advanced_projects_in_CSS_and_JS/sec9_project_1/index.js
+++ 4_advanced_projects_in_CSS_and_JS/sec9_project_1/index.js
@@ -2,19 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hello world!');
 
     const rootElement = document.querySelector('#root');
-    const sections = document.querySelectorAll('sections');
+    const sections = document.querySelectorAll('section');
     let currentSectionIndex = 0;
+    let isThrottled = false;
 
     document.addEventListener('mousewheel', function (event) {
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(function() {
+            isThrottled = false;
+        }, 1000);
+
         const direction = event.wheelDelta < 0 ? 1 : -1;
+        console.log(event.wheelDelta)
         if (direction === 1) {
-            const isLastection = currentSectionIndex === sections.length - 1;
+            const isLastSection = currentSectionIndex === sections.length - 1;
             if (isLastSection) return;
         } else if (direction === -1) {
             const firstSection = currentSectionIndex === 0;
             if (firstSection) return;
         }
         currentSectionIndex = currentSectionIndex + direction;
-        console.log(event.whellDelta)
+        console.log(currentSectionIndex);
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
     })
 })
