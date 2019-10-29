26d2085144b85437201a1251fd65863a50514ac4
AndriiChemerys <andriichemerys@gmail.com>
2019-10-29 20:27:38 +0100

    adding searchElements func

--- 4_advanced_projects_in_CSS_and_JS/sec7_JS_and_DOM/main.js
+++ 4_advanced_projects_in_CSS_and_JS/sec7_JS_and_DOM/main.js
@@ -12,3 +12,18 @@ const addElement = (e, node, txt, attr, value) => {
   document.querySelector('.content').appendChild(element);
 
 }
+
+const searchElements = (event, nameElement) => {
+  event.preventDefault();
+  const infoElement = document.querySelector('.result');
+  infoElement.textContent = '';
+  const elements = document.querySelectorAll(nameElement);
+  if (elements.length) {
+    infoElement.innerHTML = `<p class="result__number-info">W tym dokumencie znalazłem ${elements.length} elementów <strong>${nameElement}</strong></p>`;
+    showInfo(elements, infoElement)
+  } else {
+    infoElement.innerHTML = `<p class="result__number-info">W tym dokumencie nie znalazłem elementów <strong>${nameElement}</strong></p>`;
+    return;
+  }
+
+}
