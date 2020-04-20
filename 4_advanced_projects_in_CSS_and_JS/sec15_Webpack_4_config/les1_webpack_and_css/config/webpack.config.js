506f63a6290aed9dacde1df1627f8d6ca2f7a427
AndriiChemerys <andriichemerys@gmail.com>
2020-04-20 17:24:29 +0200

    webpack and css

---
+++ 4_advanced_projects_in_CSS_and_JS/sec15_Webpack_4_config/les1_webpack_and_css/config/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+// import path from 'path';
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name]-[contenthash].js',
+    path: path.resolve(__dirname, '../', 'build')
+  },
+  devServer: {
+    open: true,
+    contentBase: path.resolve(__dirname, '../', 'public'),
+    port: 5001
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "src/templates/template.html",
+      title: "nowa aplikacja"
+    }),
+
+  ]
+}
