9191785b411d7bb794926920237d8cebb6fac2ec
AndriiChemerys <andriichemerys@gmail.com>
2020-04-21 16:29:21 +0200

    img in webpack

---
+++ 4_advanced_projects_in_CSS_and_JS/sec15_Webpack_4_config/les5_img/config/webpack.config.prod.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: 'js/[name]-[contenthash].js',
+    path: path.resolve(__dirname, '../', 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|png|svg|gif|jpeg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name][contenthash:6].[ext]',
+          outputPath: 'images',
+        }
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "src/templates/template.html",
+      title: "nowa aplikacja"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash].css'
+    })
+  ]
+}
