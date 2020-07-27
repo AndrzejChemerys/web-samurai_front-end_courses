d4a6633c6f1f621b1aa2e77ce887c686a6c9f0d6
AndriiChemerys <andriichemerys@gmail.com>
2020-07-27 20:56:29 +0200

    Creating getRandomImage function

--- 4_advanced_projects_in_CSS_and_JS/sec16_API_project/les2_project/js/app.js
+++ 4_advanced_projects_in_CSS_and_JS/sec16_API_project/les2_project/js/app.js
@@ -1,1 +1,18 @@
 import '../sass/style.scss';
+
+function listBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then(resp => resp.json())
+        .then(data => data.message);
+}
+
+function getRandomImage() {
+    return fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(data => data.message);
+}
+
+const imgTag = document.querySelector('img');
+
+getRandomImage()
+    .then(imgSrc => imgTag.setAttribute('src', imgSrc));
