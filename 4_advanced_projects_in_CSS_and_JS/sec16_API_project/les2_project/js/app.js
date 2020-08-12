b74ea34df7cad3ae412fc0874c1dfd89f9243944
AndriiChemerys <andriichemerys@gmail.com>
2020-08-12 17:17:31 +0200

    Adding class Doggo

--- 4_advanced_projects_in_CSS_and_JS/sec16_API_project/les2_project/js/app.js
+++ 4_advanced_projects_in_CSS_and_JS/sec16_API_project/les2_project/js/app.js
@@ -1,24 +1,37 @@
 import '../sass/style.scss';
 
-function listBreeds() {
-    fetch('https://dog.ceo/api/breeds/list/all')
-        .then(resp => resp.json())
-        .then(data => data.message);
+class Doggo {
+    constructor() {
+        this.apiUrl = 'https://dog.ceo/api';
+        this.imgEl = document.querySelector('.featured-dog img');
+        this.backgroundEl = document.querySelector('.featured-dog__background');
+        this.tilesEl = document.querySelector('.tiles');
+        this.spinnerEl = document.querySelector('.spinner');
+
+        this.init();
+    }
+
+    listBreeds() {
+        fetch('https://dog.ceo/api/breeds/list/all')
+            .then(resp => resp.json())
+            .then(data => data.message);
+    }
+
+    getRandomImage() {
+        return fetch('https://dog.ceo/api/breeds/image/random')
+            .then(resp => resp.json())
+            .then(data => data.message);
+    }
+
+    getRandomImageByBreed(breed) {
+        return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+            .then(resp => resp.json())
+            .then(data => data.message);
+    }
+
+    const imgTag = document.querySelector('img');
+
+    getRandomImageByBreed('boxer')
+        .then(imgSrc => imgTag.setAttribute('src', imgSrc));
+
 }
-
-function getRandomImage() {
-    return fetch('https://dog.ceo/api/breeds/image/random')
-        .then(resp => resp.json())
-        .then(data => data.message);
-}
-
-function getRandomImageByBreed(breed) {
-    return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
-        .then(resp => resp.json())
-        .then(data => data.message);
-}
-
-const imgTag = document.querySelector('img');
-
-getRandomImageByBreed('boxer')
-    .then(imgSrc => imgTag.setAttribute('src', imgSrc));
