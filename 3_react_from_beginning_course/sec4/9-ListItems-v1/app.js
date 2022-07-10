f9e43430d48d56025abffc21aee02e21258e9e70
AndriiChemerys <andriichemerys@gmail.com>
2022-07-10 16:21:52 +0200

    Projekt 3: praca z tablicami, cz. 4: sortowanie po płci

--- 3_react_from_beginning_course/sec4/9-ListItems-v1/app.js
+++ 3_react_from_beginning_course/sec4/9-ListItems-v1/app.js
@@ -4,38 +4,44 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
+      sex: "male",
     },
     {
       id: 2,
       age: 49,
       name: "Marta",
+      sex: "female",
     },
     {
       id: 3,
       age: 19,
       name: "Stasia",
+      sex: "female",
     },
     {
       id: 4,
       age: 24,
       name: "Karol",
+      sex: "male",
     },
   ],
 };
 
 const Item = ({ user }) => (
-  <div>
-    <h1>Użytkownik {user.name}</h1>
-    <h2>Ma {user.age}</h2>
+  <div className="userInfo">
+    <h1>{user.name}</h1>
+    <p>Informacje o użytkowniku</p>
+    <p>
+      Wiek użytkownika: <strong>{user.age}</strong>
+    </p>
+    <p>Płeć użytkownika: {user.sex}</p>
   </div>
 );
-
 class ListItems extends React.Component {
-  // state = {
-  //   items: ["jabłko", "sliwka", "gruszka"],
-  // };
+  state = {};
   render() {
-    const users = this.props.data.users;
+    let users = this.props.data.users;
+    users = users.filter((user) => user.sex === "female");
     const Items = users.map((user) => <Item key={user.id} user={user} />);
 
     return <ul>{Items}</ul>;
