bb98b5b2a96d9445eab80d83cb16882e67ded62e
AndriiChemerys <andriichemerys@gmail.com>
2022-07-12 21:01:12 +0200

    Projekt 4: interaktywne menu restauracji, cz. 4 - logika aplikacji

--- 3_react_from_beginning_course/sec4/14-17-Order-Restaurant/Item.js
+++ 3_react_from_beginning_course/sec4/14-17-Order-Restaurant/Item.js
@@ -1,3 +1,10 @@
 const Item = (props) => (
-  <li style={props.active ? { fontWeight: "bold" } : { color: "gray" }}>{props.name}</li>
+  <li
+    className={props.active ? "enabled" : "disabled"}
+    onClick={() => props.changeStatus(props.id)}
+  >
+    {props.name}
+  </li>
 );
+
+// style={props.active ? { fontWeight: "bold" } : { color: "gray" }}
