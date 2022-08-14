b2447ee50d31a53ce83d00caea8009ac1eb86782
AndriiChemerys <andriichemerys@gmail.com>
2022-08-14 12:57:58 +0200

    Wstrzymaj 88. Funkcja walidująca wypełnianie formularza, reset poprawnie wysłanego formularza

--- 3_react_from_beginning_course/sec8/les1 - form validation/app/src/App.js
+++ 3_react_from_beginning_course/sec8/les1 - form validation/app/src/App.js
@@ -43,7 +43,73 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log("It works!");
+
+    const validation = this.formValidation();
+    console.log(validation);
+
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        pass: "",
+        accept: false,
+
+        errors: {
+          username: false,
+          email: false,
+          pass: false,
+          accept: false,
+        },
+      });
+      console.log("Form Send");
+    } else {
+      this.setState({
+        errors: {
+          username: false,
+          email: false,
+          pass: false,
+          accept: false,
+        },
+      });
+    }
+  };
+
+  formValidation = () => {
+    // true - ok
+    // false - zle
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+
+    if (this.state.username.length > 10 && this.state.username.indexOf(" ") === -1) {
+      username = true;
+    }
+
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+
+    if (this.state.pass.length === 8) {
+      password = true;
+    }
+
+    if (this.state.accept) {
+      accept = true;
+    }
+
+    if (username && email && password && accept) {
+      correct = true;
+    }
+
+    return {
+      correct,
+      username,
+      email,
+      password,
+      accept,
+    };
   };
 
   render() {
