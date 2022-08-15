e4ad6daec36fe15125310e57fcb073b184a82157
AndriiChemerys <andriichemerys@gmail.com>
2022-08-15 13:31:40 +0200

    94. Wyświetlanie zadań w sekcjach do zrobienia i zrobionych

--- 3_react_from_beginning_course/sec9/les1/app/src/components/TaskList.js
+++ 3_react_from_beginning_course/sec9/les1/app/src/components/TaskList.js
@@ -2,19 +2,36 @@ import React from "react";
 import Task from "./Task";
 
 const TaskList = (props) => {
-  const tasks = props.tasks.map((task) => (
+  // const active = props.tasks.filter(task => task.active === true)
+  const active = props.tasks.filter((task) => task.active);
+  const done = props.tasks.filter((task) => !task.active);
+  console.log(active, done);
+  const activeTasks = active.map((task) => (
     <Task key={task.id} task={task} delete={props.delete} change={props.change} />
   ));
+  const doneTasks = done.map((task) => (
+    <Task key={task.id} task={task} delete={props.delete} change={props.change} />
+  ));
+
+  // const tasks = props.tasks.map((task) => (
+  //   <Task key={task.id} task={task} delete={props.delete} change={props.change} />
+  // ));
 
   return (
     <>
       <div className="active">
         <h1>Tasks to do:</h1>
-        {tasks}
+        {activeTasks.length > 0 ? activeTasks : <p>No todos</p>}
 
         <hr />
         <div className="done">
-          <h3>Tasks done (0)</h3>
+          <h3>
+            Tasks done <em>({done.length})</em>
+          </h3>
+          {/* {doneTasks} */}
+          {/* showing just a few tasks: */}
+          {done.length > 2 && <span style={{ fontSize: 10 }}>Showing only 2 last elements:</span>}
+          {doneTasks.slice(0, 2)}
         </div>
       </div>
     </>
