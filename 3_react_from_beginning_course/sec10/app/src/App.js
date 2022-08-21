2c089bb3f1b915603c1ebee021ef0c8fdb34e078
AndriiChemerys <andriichemerys@gmail.com>
2022-08-21 18:35:08 +0200

    102. Projekt SPA z menu i różnymi adresami URL - BrowserRouter i Link

---
+++ 3_react_from_beginning_course/sec10/app/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Link } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                {/* <li>
+                  <a href="/">Start</a>
+                </li>
+                <li>
+                  <a href="/news">News</a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li> */}
+                <li>
+                  <Link to="/">Start</Link>
+                </li>
+                <li>
+                  <Link to="/news">News</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <section>Strona - hello!</section>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
